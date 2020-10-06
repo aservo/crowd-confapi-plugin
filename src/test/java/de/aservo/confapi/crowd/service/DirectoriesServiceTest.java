3823b20aefac36c402e9be424c87c9c478445b8b
Patrick Hobusch <patrick.hobusch@gmail.com>
2020-10-06 14:34:58 +0800

    WIP

--- src/test/java/de/aservo/confapi/crowd/service/DirectoriesServiceTest.java
+++ src/test/java/de/aservo/confapi/crowd/service/DirectoriesServiceTest.java
@@ -1,23 +1,29 @@
 package de.aservo.confapi.crowd.service;
 
+import com.atlassian.crowd.embedded.api.Directory;
+import com.atlassian.crowd.embedded.api.DirectoryType;
 import com.atlassian.crowd.embedded.api.MockDirectoryInternal;
 import com.atlassian.crowd.exception.DirectoryNotFoundException;
 import com.atlassian.crowd.manager.directory.DirectoryManager;
 import com.atlassian.crowd.search.query.entity.EntityQuery;
+import de.aservo.confapi.commons.exception.NotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DirectoriesServiceTest {
@@ -44,16 +50,54 @@ public class DirectoriesServiceTest {
         assertNotNull(directoriesService.getDirectory(1L));
     }
 
-    @Test
-    public void testGetDirectoryNullWhenNotFoundWithNull() throws DirectoryNotFoundException {
-        doReturn(null).when(directoryManager).findDirectoryById(anyLong());
+    @Test(expected = NotFoundException.class)
+    public void testGetDirectoryNotFoundException() throws DirectoryNotFoundException {
+        doThrow(new DirectoryNotFoundException("Directory")).when(directoryManager).findDirectoryById(anyLong());
         assertNull(directoriesService.getDirectory(1L));
     }
 
     @Test
-    public void testGetDirectoryNullWhenNotFoundWithException() throws DirectoryNotFoundException {
-        doThrow(new DirectoryNotFoundException("Directory")).when(directoryManager).findDirectoryById(anyLong());
-        assertNull(directoriesService.getDirectory(1L));
+    public void testDirectoryComparator() {
+        final List<Directory> directories = new ArrayList<>();
+
+        {
+            final Directory directoryInternal1 = mock(Directory.class);
+            doReturn(1L).when(directoryInternal1).getId();
+            doReturn("Old Internal Directory").when(directoryInternal1).getName();
+            doReturn(DirectoryType.INTERNAL).when(directoryInternal1).getType();
+            doReturn(toDate(LocalDate.now().minusDays(3))).when(directoryInternal1).getCreatedDate();
+            directories.add(directoryInternal1);
+        }
+
+        {
+            final Directory directoryAzureAd = mock(Directory.class);
+            doReturn(2L).when(directoryAzureAd).getId();
+            doReturn("Azure AD Directory").when(directoryAzureAd).getName();
+            doReturn(DirectoryType.AZURE_AD).when(directoryAzureAd).getType();
+            doReturn(toDate(LocalDate.now().minusDays(1))).when(directoryAzureAd).getCreatedDate();
+            directories.add(directoryAzureAd);
+        }
+
+        {
+            final Directory directoryInternal2 = mock(Directory.class);
+            doReturn(3L).when(directoryInternal2).getId();
+            doReturn("New Internal Directory").when(directoryInternal2).getName();
+            doReturn(DirectoryType.INTERNAL).when(directoryInternal2).getType();
+            doReturn(toDate(LocalDate.now())).when(directoryInternal2).getCreatedDate();
+            directories.add(directoryInternal2);
+        }
+
+        directories.sort(new DirectoriesServiceImpl.DirectoryComparator());
+
+        assertEquals(2L, (long) directories.get(0).getId());
+        assertEquals(3L, (long) directories.get(1).getId());
+        assertEquals(1L, (long) directories.get(2).getId());
+    }
+
+    private Date toDate(
+            final LocalDate localDate) {
+
+        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
     }
 
 }
